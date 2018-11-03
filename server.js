3db2fb032802581e6746f264504ed069ef916435
JackR <JackRTolley@gmail.com>
2018-11-03 21:02:46 +0000

    change port to 8080

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
-server.listen(5000, function() {
+server.listen(8080, function() {
   console.log('Starting server on port 5000');
 });
